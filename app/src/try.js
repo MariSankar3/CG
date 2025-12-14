4c048443f875d4a46b309f4a1ab146331becbc4d
MariSankar <sathishsankar046@gmail.com>
2025-12-14 10:58:45 +0530

    Revert "Tryjs changes"
    This reverts commit 17ee444b59cd8cd239fc675fe98a9d552b2e7564.

--- app/src/try.js
+++ app/src/try.js
@@ -38,7 +38,7 @@ function Try() {
             num: "02",
             title: "Built for safety first",
             desc:
-              "AI Doctor avoids harmful or risky recommendations and escalates to clinician.",
+              "AI Doctor avoids harmful or risky recommendations and escalates to clinicians.",
           },
           {
             num: "03",
